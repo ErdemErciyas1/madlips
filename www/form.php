7cd8db138121c594545393a8263b98737247a664
Erdem Erciyas <erdem_kirsehir_40@hotmail.com>
2017-06-03 19:26:45 +0200

    madlips bijna af

---
+++ www/form.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+  <title>Formulier</title>
+  <link rel="stylesheet" type="text/css" href="theme.css" />
+<div id="flexcontainer">
+<img src="imgs/logo.png" class=logo/>
+<h3> Er heerst paniek...	 Onkunde</h3>
+
+</head>
+
+<body>
+
+  <form method="post" action="mood.php">
+
+
+ 
+  	<div class="tekstall">
+  	<h1> Er heerst paniek...</h1>
+  <p>
+      <label>Welk dier zou je nooit als huisdier willen?</label>
+      <input type="text" name="huisdier" />
+
+    </p>
+
+    <p>
+      <label>Wie is de belangrijkste persoon in je leven?</label>
+      <input type="text" name="persoon" />
+    </p>
+
+
+    <p>
+      <label>in welk land zou je willen wonen?</label>
+      <input type="text" name="land" />
+    </p>
+
+    <p>
+      <label>Wat doe je als je je verveelt?</label>
+      <input type="text" name="vervelen" />
+
+    </p>
+    
+    <p>
+      <label>Met welk speelgoed speelde je als kind het meest?</label>
+      <input type="text" name="speelgoed" />
+    </p>
+
+    <p>
+      <label>Bij welke docent spijbel je het liefst?</label>
+      <input type="text" name="docent" />
+
+    </p>
+
+
+    <p>
+      <label>Als je 100.000,-had, wat zou je dan kopen?</label>
+      <input type="text" name="kopen" />
+
+    </p>
+
+    <p>
+      <label>Wat is je favoriete bezigheid?</label>
+      <input type="text" name="bezigheid" />
+
+    </p>
+    
+    <p>
+    <label>&nbsp;</label>
+      <input type="submit" value="Versturen" />
+    </p>
+  </form>
+  	</div>
+</div>
+</body>
+</html>
---
+++ www/mood.php
@@ -0,0 +1,32 @@
+<html>
+<body>
+  
+  	<h1> Er heerst paniek...</h1>
+	Er heerst paniek in het koninklijk <?php echo $_POST["land"];?>.
+
+	Koning <?php echo $_POST["docent"];?> is ten einde raad en als koning <?php echo $_POST["docent"];?>  ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?php echo $_POST["persoon"];?>.
+
+	"<?php echo $_POST["persoon"];?>! Het is een ramp! Het is een schande!"
+
+	"Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"
+
+	"Mijn <?php echo $_POST["huisdier"];?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST["speelgoed"];?> voor hem gekocht!" 
+
+	"Majesteit, uw <?php echo $_POST["huisdier"];?>. komt vast vanzelf weer terug?"
+
+	"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_POST["bezigheid"];?> leren?"
+
+	"Maar Sire, daar kunt u toch uw <?php echo $_POST["kopen"];?> voor gebruiken."
+
+	"<?php echo $_POST["persoon"];?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."
+
+	"<?php echo $_POST["vervelen"];?>, Sire."
+
+
+
+
+  
+    
+  
+</body>
+</html>
